d99f96de32b4503db6a3ef245c957e3b78459e63
NelsonLe <69988953+NelsonLe@users.noreply.github.com>
2020-08-21 11:14:19 -0400

    Fixed data collection process
    Requests at time points made only once now so the problem of concatenating duplicate data to the existing data is solved.

--- main2.py
+++ main2.py
@@ -10,30 +10,36 @@ startTime = time.time()
 
 # Init
 currentTime = 0
+collected10 = False
+collected15 = False
+collected20 = False
 
 # Timer loop
 while True:
-    # Request from Riot 2/sec
+    # Request 2/sec
     if time.time() - startTime >= .5:
         startTime += .5
         currentTime = int(requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify='riotgames.pem').json()['gameData']['gameTime'])
 
     # Match data at 10 mins
-    if currentTime == 60*10:
+    if currentTime == 60*10 and not collected10:
         currentMatch = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify='riotgames.pem')
         min10.write(currentMatch.text)
+        collected10 = True
         print("10 mins")
 
     # Match data at 15 mins
-    if currentTime == 60*15:
+    if currentTime == 60*15 and not collected15:
         currentMatch = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify='riotgames.pem')
         min15.write(currentMatch.text)
+        collected15 = True
         print("15 mins")
 
     # Match data at 20 mins
-    if currentTime == 60*20:
+    if currentTime == 60*20 and not collected20:
         currentMatch = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify='riotgames.pem')
         min20.write(currentMatch.text)
+        collected20 = True
         print("20 mins")
         break
 
